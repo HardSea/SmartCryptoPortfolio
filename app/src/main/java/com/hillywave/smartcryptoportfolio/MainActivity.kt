90b02db629ea6ca99155e9cdfb1ff837cc78df20
HardSea <androidmen2000@gmail.com>
2021-05-28 18:15:52 +0300

    Create Bottom Bar with buttons

--- app/src/main/java/com/hillywave/smartcryptoportfolio/MainActivity.kt
+++ app/src/main/java/com/hillywave/smartcryptoportfolio/MainActivity.kt
@@ -5,12 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import com.hillywave.smartcryptoportfolio.home.ui.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+	private val tabIcons = listOf(
+		R.drawable.ic_list,
+		R.drawable.ic_star,
+		R.drawable.ic_settings,
+		R.drawable.ic_people
+	)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
 		supportFragmentManager
 			.beginTransaction()
 			.replace(R.id.container, HomeFragment.newInstance())
 			.commit()
+
 	}
 }
