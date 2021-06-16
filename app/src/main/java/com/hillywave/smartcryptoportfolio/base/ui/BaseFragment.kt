6014755631505ea41b7b1154804408a82a0c4b70
HardSea <androidmen2000@gmail.com>
2021-06-16 22:43:11 +0300

    Create empty investing tab fragment

--- app/src/main/java/com/hillywave/smartcryptoportfolio/base/ui/BaseFragment.kt
+++ app/src/main/java/com/hillywave/smartcryptoportfolio/base/ui/BaseFragment.kt
@@ -3,4 +3,7 @@ package com.hillywave.smartcryptoportfolio.base.ui
 import androidx.fragment.app.Fragment
 
 abstract class BaseFragment(layoutId: Int) : Fragment(layoutId) {
+
+
+
 }
---
+++ app/src/main/java/com/hillywave/smartcryptoportfolio/home/investingtab/ui/InvestingTabFragment.kt
@@ -0,0 +1,10 @@
+package com.hillywave.smartcryptoportfolio.home.investingtab.ui
+
+import com.hillywave.smartcryptoportfolio.R
+import com.hillywave.smartcryptoportfolio.base.ui.BaseFragment
+
+class InvestingTabFragment : BaseFragment(R.layout.investing_tab_fragment) {
+
+
+
+}
